6a814761af663165066a73d1e73d8371ccb4a88b
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2020-04-07 15:14:51 +0300

    Lesson 7

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      hi hi hihihi
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+    );
+}
+
+const Technologies = () => {
+  return (
+    <div>
       <ul>
         <li>css</li>
         <li>html</li>
+        <li>js</li>
         <li>react</li>
       </ul>
     </div>
   );
 }
 
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">>News Feed</a>
+      <a href="#">>Messeges</a>
+    </div>
+  );
+}
 export default App;
