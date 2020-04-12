4fbe3ae69c69fdad93aeab45051de94f8e73cbb5
Bogdan2120 <bogdanvasilik2120@gmail.com>
2020-04-12 14:31:24 +0300

    lessons 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/a/ac/Oikya_Front_Logo.png'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://base.imgix.net/files/base/ebm/machinedesign/image/2019/04/machinedesign_16188_promo_eclipse.png?auto=format&fit=crop&h=432&w=768'/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                post
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
