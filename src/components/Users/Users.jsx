69331fb18e111a8dca0ea44be8d5a17c0e067e32
Bogdan2120 <bogdanvasilik2120@gmail.com>
2020-05-28 18:05:36 +0300

    lessons 50 API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,49 +1,57 @@
 import React from "react";
 import s from "./Users.module.css"
+import * as axios from "axios";
+import userImg from "../../assets/images/user.jpg"
+
 
 let Users = (props) => {
     if (props.users.length === 0) {
-     props.setUsers([
-            {
-                id: 1,
-                avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP1661-CUSA11738_00-AV00000000000022/image?_version=00_09_000&platform=chihiro&w=330&h=330&bg_color=000000&opacity=100',
-                fullName: 'Anton',
-                status: 'Student',
-                followed: true,
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-            {
-                id: 2,
-                avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP1661-CUSA11738_00-AV00000000000022/image?_version=00_09_000&platform=chihiro&w=330&h=330&bg_color=000000&opacity=100',
-                fullName: 'Vika',
-                status: 'Boss',
-                followed: false,
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 3,
-                avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP1661-CUSA11738_00-AV00000000000022/image?_version=00_09_000&platform=chihiro&w=330&h=330&bg_color=000000&opacity=100',
-                fullName: 'Artur',
-                status: 'Barder',
-                followed: true,
-                location: {city: 'Lviv', country: 'Ukraine'}
-            }
-        ])
-    }
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                debugger;
+                props.setUsers(response.data.items)
+        } )
 
+    }
+    /*props.setUsers([
+                {
+                    id: 1,
+                    avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP1661-CUSA11738_00-AV00000000000022/image?_version=00_09_000&platform=chihiro&w=330&h=330&bg_color=000000&opacity=100',
+                    fullName: 'Anton',
+                    status: 'Student',
+                    followed: true,
+                    location: {city: 'Kiev', country: 'Ukraine'}
+                },
+                {
+                    id: 2,
+                    avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP1661-CUSA11738_00-AV00000000000022/image?_version=00_09_000&platform=chihiro&w=330&h=330&bg_color=000000&opacity=100',
+                    fullName: 'Vika',
+                    status: 'Boss',
+                    followed: false,
+                    location: {city: 'Minsk', country: 'Belarus'}
+                },
+                {
+                    id: 3,
+                    avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP1661-CUSA11738_00-AV00000000000022/image?_version=00_09_000&platform=chihiro&w=330&h=330&bg_color=000000&opacity=100',
+                    fullName: 'Artur',
+                    status: 'Barder',
+                    followed: true,
+                    location: {city: 'Lviv', country: 'Ukraine'}
+                }
+            ])*/
     return (
         <div className={s.container}>
             {
                 props.users.map(u => <div className={s.userItem} key={u.id}>
                     <div className={s.avatar}>
-                        <img src={u.avatar} alt={u.fullName}/>
+                        <img src={ u.photos.small !=null ? u.photos.small : userImg } alt={u.name}/>
                     </div>
                     <div className={s.information}>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                         <div>
-                            <span>City: {u.location.city} </span>
-                            <span>Country: {u.location.country}</span>
+                            <span>City: {"u.location.city"} </span>
+                            <span>Country: {"u.location.country"}</span>
                         </div>
                     </div>
                     <div className={s.button}>
